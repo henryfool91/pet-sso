17b4c8396dea33e594cefc68a1410e42fb78c798
henry_fool <henry_fool91@gmail.com>
2026-01-16 00:07:08 +0300

    feat: implement all auth grpc methods

--- internal/grpc/auth/server.go
+++ internal/grpc/auth/server.go
@@ -10,27 +10,40 @@ import (
 	"google.golang.org/grpc/status"
 )
 
+type Auth interface {
+	Login(ctx context.Context,
+		email string,
+		password string,
+		appID int32) (token string, err error)
+
+	Register(ctx context.Context,
+		email string,
+		password string,
+	) (userID int64, err error)
+	IsAdmin(ctx context.Context,
+		userID int64) (isAdmin bool, err error)
+}
+
 type serverAPI struct {
 	ssov1.UnimplementedAuthServer
+	auth Auth
 }
 
-func Register(gRPC *grpc.Server) {
-	ssov1.RegisterAuthServer(gRPC, &serverAPI{})
+func Register(gRPC *grpc.Server, auth Auth) {
+	ssov1.RegisterAuthServer(gRPC, &serverAPI{auth: auth})
 }
 
+// Login implements [ssov1.AuthServer].
 func (s *serverAPI) Login(ctx context.Context, req *ssov1.LoginRequest) (*ssov1.LoginResponse, error) {
 	if err := protovalidate.Validate(req); err != nil {
 		return nil, status.Error(codes.InvalidArgument, err.Error())
 	}
-	return &ssov1.LoginResponse{Token: "1234"}, nil
-}
-
-// IsAdmin implements [ssov1.AuthServer].
-func (s *serverAPI) IsAdmin(ctx context.Context, req *ssov1.IsAdminRequest) (*ssov1.IsAdminResponse, error) {
-	if err := protovalidate.Validate(req); err != nil {
-		return nil, status.Error(codes.InvalidArgument, err.Error())
+	token, err := s.auth.Login(ctx, req.GetEmail(), req.GetPassword(), req.GetAppId())
+	if err != nil {
+		// TODO: ...
+		return nil, status.Error(codes.Internal, "internal error")
 	}
-	panic("unimplemented")
+	return &ssov1.LoginResponse{Token: token}, nil
 }
 
 // Register implements [ssov1.AuthServer].
@@ -38,5 +51,23 @@ func (s *serverAPI) Register(ctx context.Context, req *ssov1.RegisterRequest) (*
 	if err := protovalidate.Validate(req); err != nil {
 		return nil, status.Error(codes.InvalidArgument, err.Error())
 	}
-	panic("unimplemented")
+	userID, err := s.auth.Register(ctx, req.GetEmail(), req.GetPassword())
+	if err != nil {
+		// TODO: ...
+		return nil, status.Error(codes.Internal, "internal error")
+	}
+	return &ssov1.RegisterResponse{UserId: userID}, nil
+}
+
+// IsAdmin implements [ssov1.AuthServer].
+func (s *serverAPI) IsAdmin(ctx context.Context, req *ssov1.IsAdminRequest) (*ssov1.IsAdminResponse, error) {
+	if err := protovalidate.Validate(req); err != nil {
+		return nil, status.Error(codes.InvalidArgument, err.Error())
+	}
+	isAdmin, err := s.auth.IsAdmin(ctx, req.GetUserId())
+	if err != nil {
+		// TODO: ...
+		return nil, status.Error(codes.Internal, "internal error")
+	}
+	return &ssov1.IsAdminResponse{IsAdmin: isAdmin}, nil
 }
